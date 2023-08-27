73c4870156fee4451134008852aed8c1bf6652f6
강봉조 <home@gangbongjoui-MacBookAir.local>
2023-08-28 00:29:20 +0900

    기본 폴더 및 파일 구성

---
+++ src/app/(order)/order-details/[id]/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const OrderDetails = ({ params, searchParams }) => {
+  const { id } = params;
+  const { hello } = searchParams;
+
+  return (
+    <div>
+      {id}
+      <br />
+      {hello}
+    </div>
+  );
+};
+
+export default OrderDetails;
