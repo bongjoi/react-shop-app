e0af5a0db9e903aa732faa85a2fa4bd76ff3d77c
강봉조 <home@gangbongjoui-MacBookAir.local>
2023-09-02 21:11:37 +0900

    Input 컴포넌트 생성 및 스타일링

--- src/app/(auth)/login/LoginClient.jsx
+++ src/app/(auth)/login/LoginClient.jsx
@@ -5,6 +5,7 @@ import { useRouter } from 'next/navigation';
 
 import LogoPath from '@/assets/colorful.svg';
 import Loader from '@/components/loader/Loader';
+import Input from '@/components/input/Input';
 
 import styles from './Auth.module.scss';
 
@@ -36,7 +37,30 @@ const LoginClient = () => {
 
         <form className={styles.form} onSubmit={loginUser}>
           {/* Input */}
-          Input
+          <Input
+            email
+            icon="letter"
+            id="email"
+            name="email"
+            label="이메일"
+            placeholder="아이디(이메일)"
+            className={styles.control}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <Input
+            password
+            icon="lock"
+            id="password"
+            name="password"
+            label="비밀번호"
+            placeholder="비밀번호"
+            className={styles.control}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
           <div className={styles.group}>
             {/* 자동 로그인, 비밀번호 수정 */}
             자동 로그인, 비밀번호 수정
